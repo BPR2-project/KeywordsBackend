bdf042f9ae9e83df406a4a13d8450fa78ca9c515
Claudiu <75113646+143473@users.noreply.github.com>
2023-11-27 18:31:46 +0100

    key vault potential fix

--- Keywords.API/Program.cs
+++ Keywords.API/Program.cs
@@ -17,10 +17,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Key Vault
 var keyVaultUrl = new Uri(builder.Configuration["KeyVault:Url"]);
-var azureCredential = new DefaultAzureCredential(new DefaultAzureCredentialOptions()
-{
-    ManagedIdentityClientId = "b59b5b30-8d3e-4306-a9b6-d21ad92febfa"
-});
+var azureCredential = new DefaultAzureCredential();
 builder.Configuration.AddAzureKeyVault(keyVaultUrl, azureCredential);
 
 // External services
