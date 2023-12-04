6ca69b310e1729967581afffba4440c97c94785c
Agostina <75433684+Agostina-mr@users.noreply.github.com>
2023-12-04 20:21:14 +0100

    set mocks up

---
+++ Keywords.Tests.Integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,48 @@
+﻿using Keywords.Data.Repositories.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using WireMock.Server;
+
+namespace Keywords.Tests.Integration;
+
+public class CustomWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram> where TProgram : class
+{
+    public readonly Mock<IKeywordEntityRepository> KeywordsRepositoryMock = new();
+    public readonly Mock<IIndexerEntityRepository> IndexerRepositoryMock = new();
+    public readonly WireMockServer IndexerMockServer = WireMockServer.Start();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        var configValues = new Dictionary<string, string>
+        {
+            { "ConnectionStrings:KeywordsDb", "Value" },
+            { "Indexer:BaseUrl", IndexerMockServer.Url! },
+            { "Indexer:AccountId", "" }
+        };
+  
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(configValues)
+            .Build();
+
+        builder
+            .UseConfiguration(configuration)
+            .ConfigureAppConfiguration(configurationBuilder =>
+            {
+                configurationBuilder.AddInMemoryCollection(configValues);
+            })
+            .ConfigureServices(services =>
+            {
+                services.Remove(services.Single(d => d.ServiceType == typeof(IKeywordEntityRepository)));
+                services.AddSingleton(KeywordsRepositoryMock.Object);
+                
+                services.Remove(services.Single(d => d.ServiceType == typeof(IIndexerEntityRepository)));
+                services.AddSingleton(IndexerRepositoryMock.Object);
+            });
+
+        builder.UseEnvironment("Development");
+    }
+}
--- Keywords.Tests.Integration/IndexerIntegrationTest.cs
+++ Keywords.Tests.Integration/IndexerIntegrationTest.cs
@@ -1,41 +1,55 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
 using System.Web;
+using indexer_api;
 using Keywords.API.Swagger.Controllers.Generated;
 using Keywords.Data;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Keywords.Tests.Integration;
 
-public class IndexerIntegrationTest : IClassFixture<WebApplicationFactory<Program>>, IDisposable
+public class IndexerIntegrationTest : IClassFixture<CustomWebApplicationFactory<Program>>, IDisposable
 {
-    private readonly WebApplicationFactory<Program> _factory;
+    private readonly CustomWebApplicationFactory<Program> _factory;
     private readonly ITestOutputHelper _output;
 
-    private readonly string TestVideoUrl = "https://videoteststorageaccount.blob.core.windows.net/27663c85e1-input-53bd7/" +
-                                           "cut.mp4?sp=r&st=2023-12-03T19:10:07Z&se=2024-02-01T03:10:07Z&spr=https&" +
-                                           "sv=2022-11-02&sr=b&sig=%2Be1Ay2FPj2tWOB4l1Sjp7rpuIgGpRPRXbsQ0Pk7PLFc%3D";
+    private readonly string TestVideoUrl = "gkjjkk";
     private readonly Guid TestVideoId = new("212ced62-0fda-4d02-b4e9-6d9b76122791");
     
-    public IndexerIntegrationTest(WebApplicationFactory<Program> factory, ITestOutputHelper output)
+    public IndexerIntegrationTest(CustomWebApplicationFactory<Program> factory, ITestOutputHelper output)
     {
         _factory = factory;
         _output = output;
-        CleanUp();
     }
     
     public void Dispose()
     {
-        CleanUp();
         _factory.Dispose();
     }
 
     [Fact]
     public async Task IndexerIntegrationTest_Indexer_ShouldIndexVideo()
     {
+        _factory.IndexerMockServer
+            .Given(Request.Create().UsingGet()
+                .WithPath("/auth/trial/Accounts"))
+            .RespondWith(Response.Create()
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithBodyAsJson(new[] { new AccountInfo{ Id = "", Location = "", AccessToken = ""} }));
+
+        _factory.IndexerMockServer
+            .Given(Request.Create().UsingPost()
+                .WithPath("/*/Accounts/*/Videos"))
+            .RespondWith(Response.Create()
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithBodyAsJson(new IndexVideoReceipt { Id = "" }));
+        
         // Post video to indexer
         var client = _factory.CreateClient();
 
@@ -46,38 +60,30 @@ public class IndexerIntegrationTest : IClassFixture<WebApplicationFactory<Progra
             var content = await response.Content.ReadAsStringAsync();
             Assert.Fail($"Indexer failed to start: {response.StatusCode} - {content}");    
         }
-        
-        // Get indexer status
-        IndexerProgress? progress;
-        do
-        {
-            response = await client.GetAsync($"/indexer/{TestVideoId}");
-            if (response.StatusCode != HttpStatusCode.OK)
-            {
-                var content = await response.Content.ReadAsStringAsync();
-                Assert.Fail($"Failed to get Indexer status: {response.StatusCode} - {content}");    
-            }
-            
-            progress = await response.Content.ReadFromJsonAsync<IndexerProgress>();
-            Assert.NotNull(progress);
-            Assert.NotEqual(IndexerProgressState.Failed, progress.State);
-            
-            _output.WriteLine($"Indexer progress: {progress.State} - {progress.ProcessingProgress}");
-            await Task.Delay(TimeSpan.FromSeconds(2));
-        }
-        while(progress.State != IndexerProgressState.Succeeded);
-    }
-    
-    private void CleanUp()
-    {
-        var scope = _factory.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
-        var context = scope.ServiceProvider.GetRequiredService<KeywordsContext>();
 
-        var oldEntity = context.IndexerEntities.Find(TestVideoId);
-        if (oldEntity != null)
-        {
-            context.IndexerEntities.Remove(oldEntity);
-            context.SaveChanges();
-        }
+        _factory.IndexerRepositoryMock
+            .Verify(r => r.Insert(It.Is<IndexerEntity>(x => x.Id == TestVideoId), "email"),
+                Times.Once);
+        /*
+          // Get indexer status
+          IndexerProgress? progress;
+          do
+          {
+              response = await client.GetAsync($"/indexer/{TestVideoId}");
+              if (response.StatusCode != HttpStatusCode.OK)
+              {
+                  var content = await response.Content.ReadAsStringAsync();
+                  Assert.Fail($"Failed to get Indexer status: {response.StatusCode} - {content}");
+              }
+
+              progress = await response.Content.ReadFromJsonAsync<IndexerProgress>();
+              Assert.NotNull(progress);
+              Assert.NotEqual(IndexerProgressState.Failed, progress.State);
+
+              _output.WriteLine($"Indexer progress: {progress.State} - {progress.ProcessingProgress}");
+              await Task.Delay(TimeSpan.FromSeconds(2));
+          }
+          while(progress.State != IndexerProgressState.Succeeded);
+      */
     }
 }
