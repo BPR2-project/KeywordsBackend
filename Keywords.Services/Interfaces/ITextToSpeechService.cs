7a869f3b74e37de7e575d48a38a9ce5ce65d576d
Claudiu <75113646+143473@users.noreply.github.com>
2023-12-11 22:01:42 +0100

    documented interfaces (#24)

--- Keywords.Services/Interfaces/ITextToSpeechService.cs
+++ Keywords.Services/Interfaces/ITextToSpeechService.cs
@@ -4,5 +4,10 @@ namespace Keywords.Services.Interfaces;
 
 public interface ITextToSpeechService
 {
+    /// <summary>
+    /// Creates an audio file and attaches its URL to the entity
+    /// </summary>
+    /// <param name="keywordId">Id of the keyword</param>
+    /// <returns>Returns the keyword with the audioUrl attached</returns>
     Task<Keyword?> CreateAudio(Guid keywordId);
 }
